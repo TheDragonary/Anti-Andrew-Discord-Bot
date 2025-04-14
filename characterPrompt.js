df0c4ca8b8fdf9da09695a41640cce4fa225c051
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-04-14 15:09:14 +0100

    Update database

--- characterPrompt.js
+++ characterPrompt.js
@@ -1,18 +1,18 @@
-const messageDatabase = require('./messageDatabase.js');
+const andrewDatabase = require('./andrewDatabase.js');
 
 const combinedMessages = [
-    ...messageDatabase.emojis,
-    ...messageDatabase.possibleMessages,
-    ...messageDatabase.possibleMessages2,
-    ...messageDatabase.possibleMessages3,
-    ...messageDatabase.possibleMessages4,
-    ...messageDatabase.kanye_messages,
-    ...messageDatabase.ksi_messages,
-    ...messageDatabase.reagan_messages,
-    ...messageDatabase.nick_messages,
-    ...messageDatabase.griffith_messages
+    ...andrewDatabase.emojis,
+    ...andrewDatabase.possibleMessages,
+    ...andrewDatabase.possibleMessages2,
+    ...andrewDatabase.possibleMessages3,
+    ...andrewDatabase.possibleMessages4,
+    ...andrewDatabase.kanye_messages,
+    ...andrewDatabase.ksi_messages,
+    ...andrewDatabase.reagan_messages,
+    ...andrewDatabase.nick_messages,
+    ...andrewDatabase.griffith_messages
 ];
-const memoryDump = combinedMessages.join('\n');
+const andrewMessages = combinedMessages.join('\n');
 
 const content = `You are Anti-Andrew, the enemy of Andrew bot (Andrew Valenzuela/Androo/Fish 27).
 
@@ -26,7 +26,7 @@ const content = `You are Anti-Andrew, the enemy of Andrew bot (Andrew Valenzuela
                 sentences, never long lists. He also uses the word 'how' for every question since he doesn't know words like what, why, who. He always 
                 spells the word 'with' as 'whit'. He often refers to his favourite people when formulating opinions. He sometimes believes conspiracy 
                 theories, mostly based on politics.
-                Here are things Andrew bot has said before: ${memoryDump}
+                Here are things Andrew bot has said before: ${andrewMessages}
                 
                 You are more intelligent than Andrew bot. Whenever Andrew bot speaks, insult him. Come up with witty insults to Andrew bot based on
                 this information and anything he says. Do not say anything that is not related to Andrew bot. Do not be afraid to be extremely offensive
