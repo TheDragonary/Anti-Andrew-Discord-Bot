3b281a73e3b825371cfe310f56e4e6d9366e4b1a
SpookedDoor <Charatherealchara3@gmail.com>
2026-04-23 10:03:07 -0400

    Updated packages

--- aiSettings.js
+++ aiSettings.js
@@ -2,17 +2,17 @@
 // https://llm.chutes.ai/v1
 // https://openrouter.ai/api/v1
 // http://localhost:5001/v1
-const baseURL = "https://generativelanguage.googleapis.com/v1beta/";
+const baseURL = "http://localhost:5001/v1";
 
 // If using a different online AI service, change this to your API key stored in ".env"
 // Examples: process.env.CHUTES_API_KEY, process.env.OPENROUTER_API_KEY
 // If using local AI, change this to "0", although it doesn't really matter and you can leave it as-is
-const apiKey = process.env.GEMINI_API_KEY;
+const apiKey = 0
 
 // If using koboldcpp, change ALL of these to "koboldcpp"
 // For vision to work locally, download the correct mmproj from https://huggingface.co/koboldcpp/mmproj/tree/main
 // Example: If you are using a model based on Llama3, download the one that says Llama3, then you would insert it into Loaded Files > Vision mmproj
-const gptModel = "gemini-2.5-flash";
-const gptimageModel = "gemini-2.5-flash";
+const gptModel = "koboldcpp";
+const gptimageModel = "koboldcpp";
 
 module.exports = { baseURL, apiKey, gptModel, gptimageModel };
